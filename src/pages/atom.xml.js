166f0a920ec5c94249e0a8d1c835495658c56e3b
yusshi <dev@pixelog.net>
2026-07-05 22:48:30 +0900

    RSSフィード最新順にソートして本文を追加、10件に制限

--- src/pages/atom.xml.js
+++ src/pages/atom.xml.js
@@ -1,20 +1,29 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { getPostSlug } from '../utils/slug';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blog = await getCollection('blog');
+
+  const feed = blog
+                .filter(post => !post.data.draft)
+                .sort((a, b) => b.data.published_at.getTime() - a.data.published_at.getTime())
+                .slice(0, 10);
   return rss({
     title: 'Pixelog',
     description: '技術に関するメモと日記',
     site: context.site,
-    items: blog.map((post) => ({
+    items: feed.map((post) => ({
       title: post.data.title,
       pubDate: post.data.published_at,
       description: post.data.description,
       // Compute RSS link from post `id`
       // This example assumes all posts are rendered as `/blog/[id]` routes
       link: `/posts/${getPostSlug(post)}/`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
   });
 }
